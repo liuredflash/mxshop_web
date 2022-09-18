77360c48af43e021072413dae9376fe5b062ca74
liujun <522903052@qq.com>
2022-09-18 16:39:25 +0800

    增加用户列表查询接口

--- api/user.go
+++ api/user.go
@@ -1,10 +1,85 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	"mxshop_web/global/response"
+	"mxshop_web/proto"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func GetUserList(*gin.Context) {
+func HandleGrpcErrorToHttp(err error, c *gin.Context) {
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": e.Message(),
+				})
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "内部错误",
+				})
+			case codes.InvalidArgument:
+				c.JSON(http.StatusBadRequest, gin.H{
+					"msg": "参数错误",
+				})
+			case codes.Unavailable:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "用户服务不可用",
+				})
+			case codes.Unknown:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": "未知错误",
+				})
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"msg": e.Code(),
+				})
+			}
+		}
+	}
+}
+
+func GetUserList(ctx *gin.Context) {
 	zap.S().Debugf("获取用户列表")
+	ip := "127.0.0.1"
+	port := 3001
+	userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", ip, port), grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorw("[GetUserList] 连接 【用户服务失败】",
+			"msg", err.Error(),
+		)
+	}
+	//调用接口
+	userSrvClient := proto.NewUserClient(userConn)
+	rsp, err := userSrvClient.GetUserList(context.Background(), &proto.PageInfo{
+		Pn:    0,
+		PSize: 0,
+	})
+	if err != nil {
+		zap.S().Errorw("[GetUserList] 连接 【获取用户列表失败】")
+		HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		user := response.UserResponse{
+			Id:       value.Id,
+			NickName: value.NickName,
+			Birthday: time.Time(time.Unix(int64(value.BirthDay), 0)).Format("2000-1-11"),
+			Gender:   value.Gender,
+			Mobile:   value.Mobile,
+		}
+		result = append(result, user)
+
+		ctx.JSON(http.StatusOK, result)
+	}
 }
--- router/user.go
+++ router/user.go
@@ -1,7 +1,11 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"mxshop_web/api"
+
+	"github.com/gin-gonic/gin"
+)
 
 func InitUserRouter(Router *gin.RouterGroup) {
-
+	Router.GET("/user/list", api.GetUserList)
 }
