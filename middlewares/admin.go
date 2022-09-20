46b89f83f7ee3a2645f1d0aa35302884bb7c50b3
liujun <522903052@qq.com>
2022-09-20 09:26:06 +0800

    增加url权限认证

---
+++ middlewares/admin.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"mxshop_web/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IsAdminAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		claims, _ := ctx.Get("claims")
+		currentUser := claims.(*models.CustomClaims)
+		if currentUser.AuthorityId != 2 {
+			ctx.JSON(http.StatusForbidden, gin.H{
+				"msg": "无权限",
+			})
+			ctx.Abort()
+		}
+		ctx.Next()
+	}
+}
--- router/user.go
+++ router/user.go
@@ -2,11 +2,12 @@ package router
 
 import (
 	"mxshop_web/api"
+	"mxshop_web/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
-	Router.GET("/user/list", api.GetUserList)
+	Router.GET("/user/list", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList)
 	Router.POST("/user/passwd_login", api.PasswordLogin)
 }
